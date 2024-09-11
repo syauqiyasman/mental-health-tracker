a9c0d144c082690f62a26c87c8a00fd0bb159a8d
Syauqi Yasman <syauqiyasman@icloud.com>
2024-09-11 16:05:18 +0700

    Mengerjakan tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8@9l^mmf&9c*3(o%7=j)a0@f7s0crha!3+ppoys=6#h%)#86pa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "syauqi-muhammad31-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "syauqi-muhammad31-mentalhealthtracker2.pbp.cs.ui.ac.id"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://syauqi-muhammad31-mentalhealthtracker2.pbp.cs.ui.ac.id", "https://syauqi-muhammad31-mentalhealthtracker2.pbp.cs.ui.ac.id"]
