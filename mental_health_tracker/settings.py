00ae51dd4a19528e108b63ef3cac7692c0e2fb75
Syauqi Yasman <syauqiyasman@icloud.com>
2024-09-18 15:48:05 +0700

    Selesai Tutorial 3

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8@9l^mmf&9c*3(o%7=j)a0@f7s0crha!3+ppoys=6#h%)#86pa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "syauqi-muhammad31-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
